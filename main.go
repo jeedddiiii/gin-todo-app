fd7d5c37f231cc4d63ce62a63384cf676de94e23
jeedddiiii <pendigon.once@gmail.com>
2023-11-11 16:30:25 +0700

    Add db connect

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	Controller "gin-todo-app/controller"
+	"gin-todo-app/orm"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	orm.Dbconnect()
 	r.POST("/register", Controller.Register)
 
 	r.Run(":8080")
